070f3b8aae7613f251097816ee76d0cf16bef527
Daniil Kashapov <kdv200265@gmail.com>
2024-05-29 16:25:27 +0500

    remove copy in rebuild-router

--- rebuild-router.sh
+++ rebuild-router.sh
@@ -4,6 +4,5 @@ rm -rf spqr
 git clone https://github.com/pg-sharding/spqr.git
 cd spqr || exit 1
 make build
-cp ../configs/tpcc.sql examples/
 ./spqr-router --config ../configs/router.yaml run
 cd ..
