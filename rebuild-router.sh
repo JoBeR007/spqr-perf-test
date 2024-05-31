349780ba0cf55ca954a69fb682b4aa014ec5c752
Daniil Kashapov <kdv200265@gmail.com>
2024-05-31 17:39:49 +0500

    fix path in standby.sh and rebuild without rm -rf

--- rebuild-router.sh
+++ rebuild-router.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 pkill -9 -f "spqr-router --config"
-rm -rf spqr
-git clone https://github.com/pg-sharding/spqr.git
 cd spqr || exit 1
 make build
 ./spqr-router --config ../configs/router.yaml run
