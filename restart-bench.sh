8ce7110f69fac5128135c355f9fe72b85dd73904
Daniil Kashapov <kdv200265@gmail.com>
2024-06-03 14:09:45 +0500

    fix java command

--- restart-bench.sh
+++ restart-bench.sh
@@ -1,2 +1,2 @@
 pkill -9 -f "benchbase"
-java -jar target/benchbase-spqr/benchbase.jar -b tpcc -c ../configs/tpcc_config.xml --create=true --load=true --execute=true
+java -jar benchbase-spqr/target/benchbase-spqr/benchbase.jar -b tpcc -c configs/tpcc_config.xml --create=true --load=true --execute=true
