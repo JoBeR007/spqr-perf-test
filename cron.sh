a80f567c9cffae6a6f98d889db220064445ad711
Daniil Kashapov <kdv200265@gmail.com>
2024-06-13 16:41:18 +0500

    copy tables only if data is loaded and add cron.sh without while true cycle

--- cron.sh
+++ cron.sh
@@ -64,49 +64,45 @@ if [ "$isDataLoaded" != "true" ] && [ "$isDataLoaded" != "false" ]; then
     exit 1
 fi
 
-# Main loop to check for updates
-while true; do
-  # Get the latest commit hash
-  LATEST_COMMIT_HASH=$(fetch_latest_commit_hash)
+# Get the latest commit hash
+LATEST_COMMIT_HASH=$(fetch_latest_commit_hash)
 
-  # Check if there is a new commit
-  if [ "$LATEST_COMMIT_HASH" != "$LAST_PROCESSED_COMMIT_HASH" ]; then
+# Check if there is a new commit
+if [ "$LATEST_COMMIT_HASH" != "$LAST_PROCESSED_COMMIT_HASH" ]; then
 
-    echo "New commit detected. Running the scripts"
+  echo "New commit detected. Running the scripts"
 
-    bash "${SCRIPTS[0]}"
-    if [ $? -ne 0 ]; then
-            echo "Script ${SCRIPTS[0]} failed. Stopping the execution of further scripts."
-            exit 1
-    fi
-    echo "Starting router"
-    bash "${SCRIPTS[1]}" &
-
-    echo "Loading Data"
-    if [ "$isDataLoaded" = false ] ; then
-      scp ${REMOTE_SCRIPT1} "${BENCH_USER}"@"${BENCH_IP}":/home/spqr-perf-test/
-      ssh "${BENCH_USER}"@"${BENCH_IP}" "bash ${REMOTE_SCRIPT1}"
-      if [ $? -ne 0 ]; then
-          echo "Script ${REMOTE_SCRIPT1} failed. Stopping the execution of further scripts."
+  bash "${SCRIPTS[0]}"
+  if [ $? -ne 0 ]; then
+          echo "Script ${SCRIPTS[0]} failed. Stopping the execution of further scripts."
           exit 1
-      fi
-      isDataLoaded=true
-    fi
-
-    if [ "$isDataLoaded" = true ] ; then
-      echo "Copying tables"
-      bash "${SCRIPTS[2]}"
-
-      echo "Starting benchmark"
-      scp ${REMOTE_SCRIPT2} "${BENCH_USER}"@"${BENCH_IP}":/home/spqr-perf-test/
-      ssh "${BENCH_USER}"@"${BENCH_IP}" "bash ${REMOTE_SCRIPT2}"
-    fi
-
-    echo "$LATEST_COMMIT_HASH" > "$LAST_COMMIT_FILE"
-
-    git --git-dir="$REPO_DIR/.git" --work-tree="$REPO_DIR" pull
-  else
-    echo "No new commits. Waiting..."
   fi
-  sleep 300  # Check every 5 minutes
-done
+  echo "Starting router"
+  bash "${SCRIPTS[1]}" &
+
+  echo "Loading Data"
+  if [ "$isDataLoaded" = false ] ; then
+    scp ${REMOTE_SCRIPT1} "${BENCH_USER}"@"${BENCH_IP}":/home/spqr-perf-test/
+    ssh "${BENCH_USER}"@"${BENCH_IP}" "bash ${REMOTE_SCRIPT1}"
+    if [ $? -ne 0 ]; then
+        echo "Script ${REMOTE_SCRIPT1} failed. Stopping the execution of further scripts."
+        exit 1
+    fi
+    isDataLoaded=true
+  fi
+
+  echo "Copying tables"
+  bash "${SCRIPTS[2]}"
+
+  echo "Starting benchmark"
+  if [ "$isDataLoaded" = true ] ; then
+    scp ${REMOTE_SCRIPT2} "${BENCH_USER}"@"${BENCH_IP}":/home/spqr-perf-test/
+    ssh "${BENCH_USER}"@"${BENCH_IP}" "bash ${REMOTE_SCRIPT2}"
+  fi
+
+  echo "$LATEST_COMMIT_HASH" > "$LAST_COMMIT_FILE"
+
+  git --git-dir="$REPO_DIR/.git" --work-tree="$REPO_DIR" pull
+else
+  echo "No new commits. Waiting..."
+fi
